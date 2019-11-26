255ea68cf77e676b3c37ff40d394c7b9f1f0623a
erikjandz <43314178+erikjandz@users.noreply.github.com>
2019-11-26 10:41:55 +0100

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -9,6 +9,59 @@
 #include "string"
 
 
+class unknown_color : public std::exception {
+public:
+   unknown_color( const std::string & name  ):
+       s{ std::string{ "unknown colcor [" } + name + "]" }
+   {}
+   const char * what() const override {
+      return s.c_str();
+   }
+private:
+   std::string s;
+};
+
+
+std::ifstream & operator>>( std::ifstream & input, sf::Color & rhs ){
+   std::string s;
+   input >> s;
+   const struct { const char * name; sf::Color color; } colors[]{
+       { "yellow", sf::Color::Yellow },
+       { "red",    sf::Color::Red },
+       . . . 
+   };
+   for( auto const & color : colors ){
+       if( color.name == s ){ 
+          rhs = color.color;
+          return input;
+       }
+   }
+   if( s == "" ){
+      throw end_of_file();
+   }
+   throw unknown_color( s );
+}
+
+
+std::ifstream & operator>>( std::ifstream & input, sf::Vector2f & rhs ){
+   char c;
+   if( ! ( input >> c )){ throw end_of_file(); }
+   if( c != '(' ){ throw invalid_position( c ); }
+
+   if( ! ( input >> rhs.x )){ . . . }
+
+   if( ! ( input >> c )){ . . . }
+   . . . 
+   if( ! ( input >> rhs.y )){ . . . }
+
+   if( ! ( input >> c )){ . . . }
+   if( c != ')' ){ throw invalid_position( c ); }
+
+   return input;
+}
+
+
+
 screen_object * screen_object_read( std::ifstream & input ){
    sf::Vector2f position;
    std::string name;
@@ -39,6 +92,18 @@ drawable * factory(std::ifstream & input){
 }
 
 int main( int argc, char *argv[] ){
+	
+	std::ifstream input( "objects.txt" );
+	try {
+		for(;;){
+			screen_objects.add( screen_object_read( input ));
+		}
+	} catch ( end_of_file ){
+		// do nothing
+	} catch ( std::exception & problem ){
+		std::cout << problem.what();
+	}
+
 	sf::RenderWindow window{ sf::VideoMode{ 640, 480 }, "SFML window" };
 
 	std::vector<drawable *> drawables = {&wall1, &wall2, &wall3, &wall4, &block, &my_ball, &my_ball2};
