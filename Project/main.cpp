b349a0cb07a8e93220360d68a5c90ee2df4ca23c
erikjandz <43314178+erikjandz@users.noreply.github.com>
2019-11-26 10:39:18 +0100

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -8,6 +8,32 @@
 #include "fstream"
 #include "string"
 
+
+screen_object * screen_object_read( std::ifstream & input ){
+   sf::Vector2f position;
+   std::string name;
+   input >> position >> name;
+
+   if( name == "CIRCLE" ){
+      . . .
+      return new circle( . . . );
+
+   } else if( name == "RECTANGLE" ){
+      . . .
+      return new rectangle( . . . );
+
+   } else if( name == "PICTURE" ){
+      . . .
+      return new picture( . . . );
+
+   } else if( name == "" ){
+      throw end_of_file();
+   }
+
+   throw unknown_shape( name );
+}
+
+
 drawable * factory(std::ifstream & input){
 	std::string
 }
